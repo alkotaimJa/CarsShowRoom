6d03e33fa76dae0a78a251e6907d163922b78863
Jawher Alkotaim <alkotaimja@Waseel.com>
2024-10-30 17:37:25 +0300

    add exception handling , for each controller
    -add resource not found exception and global exception handeler.
    -this allow not repeting the exception handling in the code

--- src/main/java/com/Cars/showRoom/controller/showRoomController.java
+++ src/main/java/com/Cars/showRoom/controller/showRoomController.java
@@ -1,7 +1,13 @@
 package com.Cars.showRoom.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Cars.showRoom.exception.ResourceNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +19,16 @@ public class showRoomController {
     public String helloShowRoom() {
         log.info("showRoomController controller called");
         return "Hello!!";
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
     
 }
